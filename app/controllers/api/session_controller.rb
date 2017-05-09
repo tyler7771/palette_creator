3107b5d7eebbb0a366cd1ee1ad58b7f84512c45f
Tyler Fields <tyler.fields777@gmail.com>
2017-05-09 11:41:13 -0700

    add users and session controllers

---
+++ app/controllers/api/session_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionController < ApplicationController
+  def create
+    @user = User.find_by_credintials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user == "password"
+      render :json => { :errors => ["Password incorrect"] }, :status => 401
+    elsif @user.nil?
+      render :json => { :errors => ["Username not found"] }, :status => 404
+    else
+      sign_in(@user)
+      @stats = Rating.statistics({id: @user.id, type: "user"}, @user, "get")
+      render './api/users/show'
+    end
+  end
+
+  def destroy
+    if current_user
+      sign_out
+      render json: {}
+    else
+      render :json => { :errors => ["User not found"] }, :status => 404
+    end
+  end
+end
