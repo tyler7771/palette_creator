3fb16a5f4f9ddf1c060dfc3a0a748a2702f91fde
Yue Gu <yuegu1993@gmail.com>
2017-05-15 15:53:17 -0700

    add session middleware

--- PaletteCreator/src/middleware/session_middleware.js
+++ PaletteCreator/src/middleware/session_middleware.js
@@ -1,10 +1,28 @@
+import { LOGIN, SIGNUP, LOGOUT, receiveCurrentUser, receiveErrors } from '../actions/session_actions.js';
+import { signup, logout, login } from '../util/session_api_util.js';
+
 const SessionMiddleware ({ getState, dispatch }) => next => action => ({
   switch(action.type){
     case LOGIN:
+      login(user).then(response => {
+        dispatch(receiveCurrentUser(response.json()));
+      })
+      .catch(error => {
+        dispatch(receiveErrors(error.json()))
+      })
       return next(action);
     case SIGNUP:
+      signup(user).then(response => {
+        dispatch(receiveCurrentUser(response.json()));
+      })
+      .catch(error => {
+        dispatch(receiveErrors(error.json());
+      })
       return next(action);
     case LOGOUT:
+      logout(user).then(response => {
+        dispatch(logout());
+      })
       return next(action);
     default:
       return next(action);
