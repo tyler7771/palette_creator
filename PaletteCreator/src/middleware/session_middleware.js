88f7cd6ddca41d092aa4e974f9f9b62c73aa7a47
Tyler Fields <tyler.fields777@gmail.com>
2017-05-16 15:37:41 -0700

    fix session middleware

--- PaletteCreator/src/middleware/session_middleware.js
+++ PaletteCreator/src/middleware/session_middleware.js
@@ -1,32 +1,32 @@
 import { LOGIN, SIGNUP, LOGOUT, receiveCurrentUser, receiveErrors } from '../actions/session_actions.js';
-import { signup, logout, login } from '../util/session_api_util.js';
+import { signup, logout, login } from '../utils/session_api_utils.js';
 
-const SessionMiddleware ({ getState, dispatch }) => next => action => ({
+const SessionMiddleware = ({ getState, dispatch }) => next => action => {
   switch(action.type){
     case LOGIN:
-      login(user).then(response => {
+      login(action.user).then(response => {
         dispatch(receiveCurrentUser(response.json()));
       })
       .catch(error => {
-        dispatch(receiveErrors(error.json()))
-      })
+        dispatch(receiveErrors(error.json()));
+      });
       return next(action);
     case SIGNUP:
-      signup(user).then(response => {
+      signup(action.user).then(response => {
         dispatch(receiveCurrentUser(response.json()));
       })
       .catch(error => {
-        dispatch(receiveErrors(error.json());
-      })
+        dispatch(receiveErrors(error.json()));
+      });
       return next(action);
     case LOGOUT:
-      logout(user).then(response => {
+      logout(action.user).then(response => {
         dispatch(logout());
-      })
+      });
       return next(action);
     default:
       return next(action);
   }
-})
+};
 
-export default SessionMiddleware
+export default SessionMiddleware;
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516200643) do
+ActiveRecord::Schema.define(version: 20170516212249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "palettes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "main_color",          null: false
+    t.string   "complementary_one"
+    t.string   "complementary_two"
+    t.string   "complementary_three"
+    t.string   "complementary_four"
+    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   create_table "pallettes", force: :cascade do |t|
     t.string   "name"
